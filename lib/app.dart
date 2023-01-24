7776c07339e49f0103bc70be19474435e6a90a00
Mochamad Firgia <mochamad@firgia.com>
2023-01-24 22:15:46 +0700

    Configure firebase

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:soca/config/config.dart';
 import 'package:soca/logic/logic.dart';
+
+/// We need to initialize app before start to the main page
+///
+/// This initialize include
+/// * initialize Firebase
+/// * Initialize Splash screen
+/// * Initialize screen orientation
+Future<void> initializeApp() async {
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  Paint.enableDithering = true;
+
+  // Initialize all asynchronous method
+  await Future.wait([
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]),
+    Firebase.initializeApp(),
+  ]);
+}
 
 class App extends StatefulWidget {
   const App({required this.title, super.key});
@@ -11,7 +33,15 @@ class App extends StatefulWidget {
   State<App> createState() => _AppState();
 }
 
-class _AppState extends State<App> {
+class _AppState extends State<App> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addObserver(this);
+    FlutterNativeSplash.remove();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-
 import 'app.dart';
 
-void main() {
+void main() async {
+  await initializeApp();
   runApp(const App(title: "Soca"));
 }
