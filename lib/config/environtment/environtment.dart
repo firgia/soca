95c79c6447861cfad4ece3b37f8723f5ae5162c1
Mochamad Firgia <mochamad@firgia.com>
2023-01-25 20:57:52 +0700

    add environtment testing

--- lib/config/environtment/environtment.dart
+++ lib/config/environtment/environtment.dart
@@ -7,12 +7,39 @@
  * Copyright (c) 2023 Mochamad Firgia
  */
 
+import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import '../../core/enum/environtment_type.dart';
 
 /// This Environment is used for use Third Party Service
 abstract class Environtment {
+  /// This dotEnv used for testing only
+  static DotEnv? _dotEnvTest;
+
+  /// Do not call this test in app
+  ///
+  /// This functions is only for testing
+  @visibleForTesting
+  static test(DotEnv testDotEnv) {
+    _dotEnvTest = testDotEnv;
+  }
+
   static EnvirontmentType _current = EnvirontmentType.development;
+
+  /// Return OneSignal App ID to access OneSignal
+  static String get onesignalAppID {
+    return (_dotEnvTest ?? dotenv).env["ONESIGNAL_APP_ID"] ?? "";
+  }
+
+  /// Return Agora App ID to access Agora
+  static String get agoraAppID {
+    return (_dotEnvTest ?? dotenv).env["AGORA_APP_ID"] ?? "";
+  }
+
+  /// Return current environtment used
+  ///
+  /// The default environtment is [EnvirontmentType.development]
+  static EnvirontmentType get current => _current;
 
   /// Set the current environtment
   ///
@@ -21,20 +48,12 @@ abstract class Environtment {
   /// The default of current environtment is [EnvirontmentType.development]
   static setCurrentEnvirontment(EnvirontmentType type) => _current = type;
 
-  /// Return oneSignal App ID to access
-  static String get onesignalAppID => dotenv.env["ONESIGNAL_APP_ID"]!;
-
-  /// Return current environtment used
-  ///
-  /// The default environtment is [EnvirontmentType.development]
-  static EnvirontmentType get current => _current;
-
   /// Return true if current environtment is for `Production`
-  static bool get isProduction => _current == EnvirontmentType.production;
+  static bool isProduction() => _current == EnvirontmentType.production;
 
   /// Return true if current environtment is for `Development`
-  static bool get isDevelopment => _current == EnvirontmentType.development;
+  static bool isDevelopment() => _current == EnvirontmentType.development;
 
   /// Return true if current environtment is for `Staging`
-  static bool get isStaging => _current == EnvirontmentType.staging;
+  static bool isStaging() => _current == EnvirontmentType.staging;
 }
---
+++ test/config/environtment/environtment_test.dart
@@ -0,0 +1,123 @@
+/*
+ * Author     : Mochamad Firgia
+ * Website    : https://www.firgia.com
+ * Repository : https://github.com/firgia/soca
+ * 
+ * Created on Wed Jan 25 2023
+ * Copyright (c) 2023 Mochamad Firgia
+ */
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:soca/config/config.dart';
+import 'package:soca/core/core.dart';
+
+import '../../mock/mock.mocks.dart';
+
+void main() {
+  late DotEnv dotEnv;
+
+  setUp(() {
+    dotEnv = MockDotEnv();
+    Environtment.test(dotEnv);
+  });
+
+  group("Fields", () {
+    group("current", () {
+      test("Should set environment to development as a default", () {
+        expect(Environtment.current, EnvirontmentType.development);
+      });
+    });
+
+    group("onesignalAppID", () {
+      test("Should return ONESIGNAL_APP_ID based on .env file", () {
+        when(dotEnv.env).thenReturn({"ONESIGNAL_APP_ID": "12345"});
+        expect(Environtment.onesignalAppID, "12345");
+      });
+
+      test(
+          "Should return empty string when ONESIGNAL_APP_ID is not available on .env file",
+          () {
+        when(dotEnv.env).thenReturn({"test": "12345"});
+        expect(Environtment.onesignalAppID, "");
+      });
+    });
+
+    group("agoraAppID", () {
+      test("Should return AGORA_APP_ID based on .env file", () {
+        when(dotEnv.env).thenReturn({"AGORA_APP_ID": "123"});
+        expect(Environtment.agoraAppID, "123");
+      });
+
+      test(
+          "Should return empty string when AGORA_APP_ID is not available on .env file",
+          () {
+        when(dotEnv.env).thenReturn({"test": "123"});
+        expect(Environtment.agoraAppID, "");
+      });
+    });
+  });
+
+  group("Functions", () {
+    group("setCurrentEnvirontment", () {
+      test(
+          "Should change the current environtment to staging when call setCurrentEnvirontment with staging parameter",
+          () {
+        Environtment.setCurrentEnvirontment(EnvirontmentType.staging);
+        expect(Environtment.current, EnvirontmentType.staging);
+      });
+
+      test(
+          "Should change the current environtment to production when call setCurrentEnvirontment with production parameter",
+          () {
+        Environtment.setCurrentEnvirontment(EnvirontmentType.production);
+        expect(Environtment.current, EnvirontmentType.production);
+      });
+
+      test(
+          "Should change the current environtment to development when call setCurrentEnvirontment with development parameter",
+          () {
+        Environtment.setCurrentEnvirontment(EnvirontmentType.development);
+        expect(Environtment.current, EnvirontmentType.development);
+      });
+    });
+
+    group("isProduction", () {
+      test("Should return true if current environtment is production", () {
+        Environtment.setCurrentEnvirontment(EnvirontmentType.production);
+        expect(Environtment.isProduction(), true);
+      });
+
+      test("Should return false if current environtment is not production", () {
+        Environtment.setCurrentEnvirontment(EnvirontmentType.development);
+        expect(Environtment.isProduction(), false);
+      });
+    });
+
+    group("isStaging", () {
+      test("Should return true if current environtment is staging", () {
+        Environtment.setCurrentEnvirontment(EnvirontmentType.staging);
+        expect(Environtment.isStaging(), true);
+      });
+
+      test("Should return false if current environtment is not staging", () {
+        Environtment.setCurrentEnvirontment(EnvirontmentType.production);
+        expect(Environtment.isStaging(), false);
+      });
+    });
+
+    group("isDevelopment", () {
+      test("Should return true if current environtment is development", () {
+        Environtment.setCurrentEnvirontment(EnvirontmentType.development);
+        expect(Environtment.isDevelopment(), true);
+      });
+
+      test("Should return false if current environtment is not development",
+          () {
+        Environtment.setCurrentEnvirontment(EnvirontmentType.production);
+        expect(Environtment.isDevelopment(), false);
+      });
+    });
+  });
+}
