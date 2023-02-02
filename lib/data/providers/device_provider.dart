a3d329d3e5f4dbdeeb70d45166b9243105026cc6
Mochamad Firgia <mochamad@firgia.com>
2023-02-02 20:01:29 +0700

    add get device ID

---
+++ lib/data/providers/device_provider.dart
@@ -0,0 +1,34 @@
+/*
+ * Author     : Mochamad Firgia
+ * Website    : https://www.firgia.com
+ * Repository : https://github.com/firgia/soca
+ * 
+ * Created on Thu Feb 02 2023
+ * Copyright (c) 2023 Mochamad Firgia
+ */
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:uuid/uuid.dart';
+
+import '../../injection.dart';
+
+class DeviceProvider {
+  String get deviceIDKey => "device_id_key";
+  final FlutterSecureStorage _secureStorage = sl<FlutterSecureStorage>();
+
+  /// Get device ID
+  ///
+  /// The device id is only generated once, so each device has a unique ID.
+  Future<String> getDeviceID() async {
+    String? deviceID = await _secureStorage.read(key: deviceIDKey);
+    if (deviceID == null) {
+      const uuid = Uuid();
+      String newDeviceID = uuid.v4();
+
+      await _secureStorage.write(key: deviceIDKey, value: newDeviceID);
+      return newDeviceID;
+    } else {
+      return deviceID;
+    }
+  }
+}
