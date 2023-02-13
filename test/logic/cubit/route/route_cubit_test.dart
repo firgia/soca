37d939207ba8d86a8ae44ee3c6f94efec1128b28
Mochamad Firgia <mochamad@firgia.com>
2023-02-13 14:14:28 +0700

    add route sign up validation

--- test/logic/cubit/route/route_cubit_test.dart
+++ test/logic/cubit/route/route_cubit_test.dart
@@ -11,6 +11,8 @@ import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:soca/config/config.dart';
+import 'package:soca/core/core.dart';
+import 'package:soca/data/data.dart';
 import 'package:soca/logic/logic.dart';
 
 import '../../../helper/helper.dart';
@@ -18,24 +20,57 @@ import '../../../mock/mock.mocks.dart';
 
 void main() {
   late MockAuthRepository authRepository;
+  late MockUserRepository userRepository;
 
   setUp(() {
     registerLocator();
     authRepository = getMockAuthRepository();
+    userRepository = getMockUserRepository();
   });
   tearDown(() => unregisterLocator());
 
   group(".getTargetRoute()", () {
     blocTest<RouteCubit, RouteState>(
-      'Should emits [RouteLoading, RouteTarget(AppPages.home)] when user has been signed in.',
+      'Should emits [RouteLoading, RouteTarget(AppPages.home)] when user has been signed in and registered.',
       build: () => RouteCubit(),
       act: (route) => route.getTargetRoute(),
       setUp: () {
         when(authRepository.isSignedIn()).thenAnswer((_) => Future.value(true));
+        when(userRepository.getProfile())
+            .thenAnswer((_) => Future.value(const User()));
       },
       expect: () => <RouteState>[
         const RouteLoading(),
         RouteTarget(AppPages.home),
+      ],
+    );
+
+    blocTest<RouteCubit, RouteState>(
+      'Should emits [RouteLoading, RouteTarget(AppPages.signUp)] when user has been signed in and not registered.',
+      build: () => RouteCubit(),
+      act: (route) => route.getTargetRoute(),
+      setUp: () {
+        when(authRepository.isSignedIn()).thenAnswer((_) => Future.value(true));
+        when(userRepository.getProfile())
+            .thenThrow(const UserFailure(code: UserFailureCode.notFound));
+      },
+      expect: () => <RouteState>[
+        const RouteLoading(),
+        RouteTarget(AppPages.signUp),
+      ],
+    );
+
+    blocTest<RouteCubit, RouteState>(
+      'Should emits [RouteLoading, RouteError] when user has been signed in and and get unknown error].',
+      build: () => RouteCubit(),
+      act: (route) => route.getTargetRoute(),
+      setUp: () {
+        when(authRepository.isSignedIn()).thenAnswer((_) => Future.value(true));
+        when(userRepository.getProfile()).thenThrow(Exception());
+      },
+      expect: () => <RouteState>[
+        const RouteLoading(),
+        RouteError(Exception()),
       ],
     );
 
--- test/logic/cubit/route/route_state_test.dart
+++ test/logic/cubit/route/route_state_test.dart
@@ -7,6 +7,7 @@
  * Copyright (c) 2023 Mochamad Firgia
  */
 
+import 'package:cloud_functions/cloud_functions.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:soca/config/config.dart';
 import 'package:soca/logic/logic.dart';
@@ -18,6 +19,19 @@ void main() {
         final routeTarget = RouteTarget(AppPages.home);
 
         expect(routeTarget.name, AppPages.home);
+      });
+    });
+  });
+
+  group("RouteError", () {
+    group(".failure", () {
+      test("Should fill up the [failure] based on the constructor parameter",
+          () {
+        final routeError = RouteError(
+            FirebaseFunctionsException(message: "test", code: "not-found"));
+
+        expect(routeError.failure,
+            FirebaseFunctionsException(message: "test", code: "not-found"));
       });
     });
   });
