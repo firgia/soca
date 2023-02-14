b615d6bcfd82116359bebb7d9b8290fd58c2ff72
Mochamad Firgia <mochamad@firgia.com>
2023-02-14 13:24:35 +0700

    add illustration card button

--- lib/presentation/widgets/button/button.dart
+++ lib/presentation/widgets/button/button.dart
@@ -11,6 +11,7 @@ export 'async_button.dart';
 export 'auth_icon_button.dart';
 export 'custom_back_button.dart';
 export 'flag_button.dart';
+export 'illustration_card_button.dart';
 export 'sign_in_button.dart';
 export 'sign_in_with_apple_button.dart';
 export 'sign_in_with_google_button.dart';
