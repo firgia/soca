2a127e3298fb2f7aad38357d49ba9d848d961df0
Mochamad Firgia <mochamad@firgia.com>
2023-02-16 20:52:47 +0700

    add language card

---
+++ lib/presentation/widgets/card/language_card.dart
@@ -0,0 +1,169 @@
+/*
+ * Author     : Mochamad Firgia
+ * Website    : https://www.firgia.com
+ * Repository : https://github.com/firgia/soca
+ * 
+ * Created on Tue Feb 14 2023
+ * Copyright (c) 2023 Mochamad Firgia
+ */
+
+import 'package:custom_icons/custom_icons.dart';
+import 'package:flutter/material.dart';
+
+import '../../../config/config.dart';
+import '../../../data/data.dart';
+import '../loading/loading.dart';
+
+class LanguageCard extends _LanguageCardItem {
+  const LanguageCard(
+    Language data, {
+    Key? key,
+    VoidCallback? onTapRemove,
+  }) : super(
+          isLoading: false,
+          data: data,
+          onTapRemove: onTapRemove,
+          key: key,
+        );
+
+  const LanguageCard.loading({Key? key})
+      : super(
+          isLoading: true,
+          key: key,
+        );
+}
+
+class _LanguageCardItem extends StatelessWidget {
+  const _LanguageCardItem({
+    required this.isLoading,
+    this.data,
+    this.onTapRemove,
+    Key? key,
+  }) : super(key: key);
+
+  final Language? data;
+  final VoidCallback? onTapRemove;
+  final bool isLoading;
+
+  @override
+  Widget build(BuildContext context) {
+    if (!isLoading) {
+      assert(data != null, "Data is required");
+    }
+    return !isLoading
+        ? _LanguageCard(
+            data!,
+            onTapRemove: onTapRemove,
+            key: key,
+          )
+        : _LanguageCardLoading(key: key);
+  }
+}
+
+class _LanguageCard extends StatelessWidget {
+  const _LanguageCard(
+    this.data, {
+    Key? key,
+    this.onTapRemove,
+  }) : super(key: key);
+
+  final Language data;
+  final VoidCallback? onTapRemove;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.zero,
+      child: Container(
+        width: double.maxFinite,
+        padding: const EdgeInsets.all(kDefaultSpacing),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _buildName(context),
+                  _buildNativeName(context),
+                ],
+              ),
+            ),
+            if (onTapRemove != null) const SizedBox(width: kDefaultSpacing),
+            if (onTapRemove != null) _buildRemoveButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildRemoveButton() {
+    return IconButton(
+      key: const Key("language_card_remove_button"),
+      onPressed: onTapRemove,
+      padding: EdgeInsets.zero,
+      constraints: const BoxConstraints(),
+      iconSize: 20,
+      icon: const Icon(CustomIcons.delete),
+    );
+  }
+
+  Widget _buildName(BuildContext context) {
+    return Text(
+      data.name ?? "-",
+      style: Theme.of(context).textTheme.bodyLarge,
+      maxLines: 1,
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+
+  Widget _buildNativeName(BuildContext context) {
+    return Text(
+      data.nativeName ?? "-",
+      style: Theme.of(context).textTheme.bodySmall,
+      maxLines: 1,
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+}
+
+class _LanguageCardLoading extends StatelessWidget {
+  const _LanguageCardLoading({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.zero,
+      child: Container(
+        width: double.maxFinite,
+        padding: const EdgeInsets.all(kDefaultSpacing),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  CustomShimmer(
+                    height: 16,
+                    width: 100,
+                  ),
+                  SizedBox(height: 8),
+                  CustomShimmer(
+                    height: 12,
+                    width: 80,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(width: kDefaultSpacing),
+            const CustomShimmer(
+              height: 20,
+              width: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
