d8a7ee5cfe71a015f6aa76e1e427844f43582d18
Mochamad Firgia <mochamad@firgia.com>
2023-03-04 16:01:36 +0700

    fixing language repository test

--- test/data/repositories/language_repository_test.dart
+++ test/data/repositories/language_repository_test.dart
@@ -28,101 +28,99 @@ void main() {
 
   tearDown(() => unregisterLocator());
 
-  group("Functions", () {
-    group("getLanguage", () {
-      test("Should convert json to [List<Language>]", () async {
-        when(localLanguageProvider.getLanguages()).thenAnswer(
-          (_) => Future.value(
-            json.decode(
-              json.encode([
-                {"code": "en", "name": "English", "nativeName": "English"},
-                {
-                  "code": "id",
-                  "name": "Indonesian",
-                  "nativeName": "Bahasa Indonesia"
-                },
-              ]),
-            ),
+  group(".getLanguage()", () {
+    test("Should convert json to [List<Language>]", () async {
+      when(localLanguageProvider.getLanguages()).thenAnswer(
+        (_) => Future.value(
+          json.decode(
+            json.encode([
+              {"code": "en", "name": "English", "nativeName": "English"},
+              {
+                "code": "id",
+                "name": "Indonesian",
+                "nativeName": "Bahasa Indonesia"
+              },
+            ]),
           ),
-        );
+        ),
+      );
 
-        final result = await languageProvider.getLanguages();
-        expect(result, const [
-          Language(
-            code: "en",
-            name: "English",
-            nativeName: "English",
-          ),
-          Language(
-            code: "id",
-            name: "Indonesian",
-            nativeName: "Bahasa Indonesia",
-          )
-        ]);
-        verify(localLanguageProvider.getLanguages());
-      });
+      final result = await languageProvider.getLanguages();
+      expect(result, const [
+        Language(
+          code: "en",
+          name: "English",
+          nativeName: "English",
+        ),
+        Language(
+          code: "id",
+          name: "Indonesian",
+          nativeName: "Bahasa Indonesia",
+        )
+      ]);
+      verify(localLanguageProvider.getLanguages());
+    });
+  });
+
+  group(".getLastChanged()", () {
+    test("Should return last DeviceLanguage data based on storage data",
+        () async {
+      when(localLanguageProvider.getLastChanged()).thenAnswer(
+        (_) => Future.value("indonesian"),
+      );
+
+      final result = await languageProvider.getLastChanged();
+      expect(result, DeviceLanguage.indonesian);
+      verify(localLanguageProvider.getLastChanged()).called(1);
     });
 
-    group("getLastChanged", () {
-      test("Should return last DeviceLanguage data based on storage data",
-          () async {
-        when(localLanguageProvider.getLastChanged()).thenAnswer(
-          (_) => Future.value("indonesian"),
-        );
+    test("Should return null when data is not found", () async {
+      when(localLanguageProvider.getLastChanged()).thenAnswer(
+        (_) => Future.value(null),
+      );
 
-        final result = await languageProvider.getLastChanged();
-        expect(result, DeviceLanguage.indonesian);
-        verify(localLanguageProvider.getLastChanged()).called(1);
-      });
+      final result = await languageProvider.getLastChanged();
+      expect(result, null);
+      verify(localLanguageProvider.getLastChanged()).called(1);
+    });
+  });
 
-      test("Should return null when data is not found", () async {
-        when(localLanguageProvider.getLastChanged()).thenAnswer(
-          (_) => Future.value(null),
-        );
+  group(".getLastChangedOnesignal()", () {
+    test("Should return last DeviceLanguage data based on storage data",
+        () async {
+      when(localLanguageProvider.getLastChangedOnesignal()).thenAnswer(
+        (_) => Future.value("indonesian"),
+      );
 
-        final result = await languageProvider.getLastChanged();
-        expect(result, null);
-        verify(localLanguageProvider.getLastChanged()).called(1);
-      });
+      final result = await languageProvider.getLastChangedOnesignal();
+      expect(result, DeviceLanguage.indonesian);
+      verify(localLanguageProvider.getLastChangedOnesignal()).called(1);
     });
 
-    group("getLastChangedOnesignal", () {
-      test("Should return last DeviceLanguage data based on storage data",
-          () async {
-        when(localLanguageProvider.getLastChangedOnesignal()).thenAnswer(
-          (_) => Future.value("indonesian"),
-        );
+    test("Should return null when data is not found", () async {
+      when(localLanguageProvider.getLastChangedOnesignal()).thenAnswer(
+        (_) => Future.value(null),
+      );
 
-        final result = await languageProvider.getLastChangedOnesignal();
-        expect(result, DeviceLanguage.indonesian);
-        verify(localLanguageProvider.getLastChangedOnesignal()).called(1);
-      });
-
-      test("Should return null when data is not found", () async {
-        when(localLanguageProvider.getLastChangedOnesignal()).thenAnswer(
-          (_) => Future.value(null),
-        );
-
-        final result = await languageProvider.getLastChangedOnesignal();
-        expect(result, null);
-        verify(localLanguageProvider.getLastChangedOnesignal()).called(1);
-      });
+      final result = await languageProvider.getLastChangedOnesignal();
+      expect(result, null);
+      verify(localLanguageProvider.getLastChangedOnesignal()).called(1);
     });
+  });
 
-    group("updateLastChanged", () {
-      test("Should save DeviceLanguage data to storage", () async {
-        await languageProvider.updateLastChanged(DeviceLanguage.indonesian);
-        verify(localLanguageProvider.updateLastChanged("indonesian")).called(1);
-      });
+  group(".updateLastChanged()", () {
+    test("Should save DeviceLanguage data to storage", () async {
+      await languageProvider.updateLastChanged(DeviceLanguage.indonesian);
+      verify(localLanguageProvider.updateLastChanged("indonesian")).called(1);
     });
+  });
 
-    group("updateLastChangedOnesignal", () {
-      test("Should save DeviceLanguage data to storage", () async {
-        await languageProvider
-            .updateLastChangedOnesignal(DeviceLanguage.indonesian);
-        verify(localLanguageProvider.updateLastChangedOnesignal("indonesian"))
-            .called(1);
-      });
+  group(".updateLastChangedOnesignal()", () {
+    test("Should save DeviceLanguage data to storage", () async {
+      await languageProvider
+          .updateLastChangedOnesignal(DeviceLanguage.indonesian);
+      verify(localLanguageProvider.updateLastChangedOnesignal("indonesian"))
+          .called(1);
     });
   });
 }
