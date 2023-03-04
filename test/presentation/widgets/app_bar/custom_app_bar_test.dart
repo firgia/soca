e99b53b35330ea7018e57e6a53639796f77dfc7e
Mochamad Firgia <mochamad@firgia.com>
2023-03-04 20:43:04 +0700

    fixing custom app bar test

--- test/presentation/widgets/app_bar/custom_app_bar_test.dart
+++ test/presentation/widgets/app_bar/custom_app_bar_test.dart
@@ -17,6 +17,92 @@ void main() {
   setUp(() => registerLocator());
   tearDown(() => unregisterLocator());
 
+  Finder findCustomBackButton() => find.byType(CustomBackButton);
+  Finder findDividerOpacity() =>
+      find.byKey(const Key("custom_app_bar_divider_opacity"));
+  Finder findLargeTitleOpacity() =>
+      find.byKey(const Key("custom_app_bar_large_title_opacity"));
+  Finder findTitleOpacity() =>
+      find.byKey(const Key("custom_app_bar_title_opacity"));
+
+  group("Actions", () {
+    testWidgets("Should show the actions", (tester) async {
+      await tester.runAsync(() async {
+        const actions = [Icon(Icons.telegram), Text("test")];
+        await tester.pumpApp(
+          child: CustomAppBar(
+            title: "hello world",
+            body: Container(),
+            actions: actions,
+          ),
+        );
+
+        for (Widget widget in actions) {
+          expect(find.byWidget(widget), findsOneWidget);
+        }
+      });
+    });
+  });
+
+  group("Body", () {
+    testWidgets("Should show the body", (tester) async {
+      await tester.runAsync(() async {
+        final widget = Container(width: 10, height: 10, color: Colors.red);
+        await tester.pumpApp(
+            child: CustomAppBar(title: "hello world", body: widget));
+
+        expect(find.byWidget(widget), findsOneWidget);
+      });
+    });
+  });
+
+  group("CustomBackButton", () {
+    testWidgets("Should show the CustomBackButton", (tester) async {
+      await tester.runAsync(() async {
+        await tester.pumpApp(
+            child: CustomAppBar(title: "hello world", body: Container()));
+        expect(findCustomBackButton(), findsOneWidget);
+      });
+    });
+  });
+
+  group("Divider", () {
+    testWidgets("Should hide the divider when not scrolling", (tester) async {
+      await tester.runAsync(() async {
+        await tester.pumpApp(
+          child: CustomAppBar(
+            title: "hello world",
+            body: Container(),
+          ),
+        );
+
+        final dividerOpacity =
+            findDividerOpacity().getWidget() as AnimatedOpacity;
+
+        expect(dividerOpacity.opacity, 0);
+      });
+    });
+
+    testWidgets("Should show the divider when scrolling", (tester) async {
+      await tester.runAsync(() async {
+        await tester.pumpApp(
+          child: CustomAppBar(
+            title: "hello world",
+            body: Container(),
+          ),
+        );
+
+        await tester.drag(find.byType(NestedScrollView), const Offset(0, -300));
+        await tester.pump();
+
+        final dividerOpacity =
+            findDividerOpacity().getWidget() as AnimatedOpacity;
+
+        expect(dividerOpacity.opacity, 1);
+      });
+    });
+  });
+
   group("Title", () {
     testWidgets("Should show the title text", (tester) async {
       await tester.runAsync(() async {
@@ -37,13 +123,10 @@ void main() {
           ),
         );
 
-        final titleOpacity = find
-            .byKey(const Key("custom_app_bar_title_opacity"))
-            .getWidget() as AnimatedOpacity;
+        final titleOpacity = findTitleOpacity().getWidget() as AnimatedOpacity;
 
-        final largeTitleOpacity = find
-            .byKey(const Key("custom_app_bar_large_title_opacity"))
-            .getWidget() as AnimatedOpacity;
+        final largeTitleOpacity =
+            findLargeTitleOpacity().getWidget() as AnimatedOpacity;
 
         expect(titleOpacity.opacity, 0);
         expect(largeTitleOpacity.opacity, 1);
@@ -64,96 +147,13 @@ void main() {
         await tester.drag(find.byType(NestedScrollView), const Offset(0, -300));
         await tester.pump();
 
-        final titleOpacity = find
-            .byKey(const Key("custom_app_bar_title_opacity"))
-            .getWidget() as AnimatedOpacity;
+        final titleOpacity = findTitleOpacity().getWidget() as AnimatedOpacity;
 
-        final largeTitleOpacity = find
-            .byKey(const Key("custom_app_bar_large_title_opacity"))
-            .getWidget() as AnimatedOpacity;
+        final largeTitleOpacity =
+            findLargeTitleOpacity().getWidget() as AnimatedOpacity;
 
         expect(titleOpacity.opacity, 1);
         expect(largeTitleOpacity.opacity, 0);
-      });
-    });
-  });
-
-  group("Divider", () {
-    testWidgets("Should hide the divider when not scrolling", (tester) async {
-      await tester.runAsync(() async {
-        await tester.pumpApp(
-          child: CustomAppBar(
-            title: "hello world",
-            body: Container(),
-          ),
-        );
-
-        final dividerOpacity = find
-            .byKey(const Key("custom_app_bar_divider_opacity"))
-            .getWidget() as AnimatedOpacity;
-
-        expect(dividerOpacity.opacity, 0);
-      });
-    });
-
-    testWidgets("Should show the divider when scrolling", (tester) async {
-      await tester.runAsync(() async {
-        await tester.pumpApp(
-          child: CustomAppBar(
-            title: "hello world",
-            body: Container(),
-          ),
-        );
-
-        await tester.drag(find.byType(NestedScrollView), const Offset(0, -300));
-        await tester.pump();
-
-        final dividerOpacity = find
-            .byKey(const Key("custom_app_bar_divider_opacity"))
-            .getWidget() as AnimatedOpacity;
-
-        expect(dividerOpacity.opacity, 1);
-      });
-    });
-  });
-
-  group("CustomBackButton", () {
-    testWidgets("Should render the CustomBackButton", (tester) async {
-      await tester.runAsync(() async {
-        await tester.pumpApp(
-            child: CustomAppBar(title: "hello world", body: Container()));
-        expect(find.byType(CustomBackButton), findsOneWidget);
-      });
-    });
-  });
-
-  group("Body", () {
-    testWidgets("Should render the body", (tester) async {
-      await tester.runAsync(() async {
-        final widget = Container(width: 10, height: 10, color: Colors.red);
-        await tester.pumpApp(
-            child: CustomAppBar(title: "hello world", body: widget));
-
-        expect(find.byWidget(widget), findsOneWidget);
-      });
-    });
-  });
-
-  group("Actions", () {
-    testWidgets("Should render the actions", (tester) async {
-      await tester.runAsync(() async {
-        const actions = [Icon(Icons.telegram), Text("test")];
-        await tester.pumpApp(
-          child: CustomAppBar(
-            title: "hello world",
-            body: Container(),
-            actions: actions,
-          ),
-        );
-
-        for (Widget widget in actions) {
-          expect(find.byWidget(widget), findsOneWidget);
-        }
       });
     });
   });
