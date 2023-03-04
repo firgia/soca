8d9361b670d5dfa7150549828f5cbf76827bd426
Mochamad Firgia <mochamad@firgia.com>
2023-03-04 15:34:49 +0700

    fixing device language extension test

--- test/core/utility/extension/device_language_extension_test.dart
+++ test/core/utility/extension/device_language_extension_test.dart
@@ -12,244 +12,241 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:soca/core/core.dart';
 
 void main() {
-  group("Functions", () {
-    group("getImage", () {
-      test("Should return Saudi Arabia flag image when DeviceLanguage.arabic",
-          () {
-        expect(
-          DeviceLanguage.arabic.getImage(),
-          const AssetImage(ImageRaster.flagSaudiArabia),
-        );
-      });
-
-      test("Should return United States flag image when DeviceLanguage.english",
-          () {
-        expect(
-          DeviceLanguage.english.getImage(),
-          const AssetImage(ImageRaster.flagUnitedStates),
-        );
-      });
-
-      test("Should return Spain flag image when DeviceLanguage.spanish", () {
-        expect(
-          DeviceLanguage.spanish.getImage(),
-          const AssetImage(ImageRaster.flagSpain),
-        );
-      });
-
-      test("Should return Indian flag image when DeviceLanguage.hindi", () {
-        expect(
-          DeviceLanguage.hindi.getImage(),
-          const AssetImage(ImageRaster.flagIndia),
-        );
-      });
-
-      test("Should return Indonesian flag image when DeviceLanguage.indonesian",
-          () {
-        expect(
-          DeviceLanguage.indonesian.getImage(),
-          const AssetImage(ImageRaster.flagIndonesia),
-        );
-      });
-
-      test("Should return Russian flag image when DeviceLanguage.russian", () {
-        expect(
-          DeviceLanguage.russian.getImage(),
-          const AssetImage(ImageRaster.flagRussia),
-        );
-      });
-
-      test("Should return Chinese flag image when DeviceLanguage.chinese", () {
-        expect(
-          DeviceLanguage.chinese.getImage(),
-          const AssetImage(ImageRaster.flagChina),
-        );
-      });
+  group(".getImage()", () {
+    test("Should return Saudi Arabia flag image when DeviceLanguage.arabic",
+        () {
+      expect(
+        DeviceLanguage.arabic.getImage(),
+        const AssetImage(ImageRaster.flagSaudiArabia),
+      );
     });
 
-    group("getName", () {
-      test("Should return 'Arabic' text when DeviceLanguage.arabic", () {
-        expect(
-          DeviceLanguage.arabic.getName(),
-          "Arabic",
-        );
-      });
-
-      test("Should return 'English' text when DeviceLanguage.english", () {
-        expect(
-          DeviceLanguage.english.getName(),
-          "English",
-        );
-      });
-
-      test("Should return 'Spanish' text when DeviceLanguage.spanish", () {
-        expect(
-          DeviceLanguage.spanish.getName(),
-          "Spanish",
-        );
-      });
-
-      test("Should return 'Hindi' text when DeviceLanguage.hindi", () {
-        expect(
-          DeviceLanguage.hindi.getName(),
-          "Hindi",
-        );
-      });
-
-      test("Should return 'Russian' text when DeviceLanguage.russian", () {
-        expect(
-          DeviceLanguage.russian.getName(),
-          "Russian",
-        );
-      });
-
-      test(
-          "Should return 'Chinese, Simplified' text when DeviceLanguage.chinese",
-          () {
-        expect(
-          DeviceLanguage.chinese.getName(),
-          "Chinese, Simplified",
-        );
-      });
+    test("Should return United States flag image when DeviceLanguage.english",
+        () {
+      expect(
+        DeviceLanguage.english.getImage(),
+        const AssetImage(ImageRaster.flagUnitedStates),
+      );
     });
 
-    group("getNativeName", () {
-      test("Should return 'عربي' text when DeviceLanguage.arabic", () {
-        expect(
-          DeviceLanguage.arabic.getNativeName(),
-          "عربي",
-        );
-      });
-
-      test("Should return 'English' text when DeviceLanguage.english", () {
-        expect(
-          DeviceLanguage.english.getNativeName(),
-          "English",
-        );
-      });
-
-      test("Should return 'Español' text when DeviceLanguage.spanish", () {
-        expect(
-          DeviceLanguage.spanish.getNativeName(),
-          "Español",
-        );
-      });
-
-      test("Should return 'हिन्दी' text when DeviceLanguage.hindi", () {
-        expect(
-          DeviceLanguage.hindi.getNativeName(),
-          "हिन्दी",
-        );
-      });
-
-      test("Should return 'Русский' text when DeviceLanguage.russian", () {
-        expect(
-          DeviceLanguage.russian.getNativeName(),
-          "Русский",
-        );
-      });
-
-      test("Should return '简体中文' text when DeviceLanguage.chinese", () {
-        expect(
-          DeviceLanguage.chinese.getNativeName(),
-          "简体中文",
-        );
-      });
+    test("Should return Spain flag image when DeviceLanguage.spanish", () {
+      expect(
+        DeviceLanguage.spanish.getImage(),
+        const AssetImage(ImageRaster.flagSpain),
+      );
     });
 
-    group("getLanguageCode", () {
-      test("Should return 'ar' text when DeviceLanguage.arabic", () {
-        expect(
-          DeviceLanguage.arabic.getLanguageCode(),
-          "ar",
-        );
-      });
-
-      test("Should return 'en' text when DeviceLanguage.english", () {
-        expect(
-          DeviceLanguage.english.getLanguageCode(),
-          "en",
-        );
-      });
-
-      test("Should return 'es' text when DeviceLanguage.spanish", () {
-        expect(
-          DeviceLanguage.spanish.getLanguageCode(),
-          "es",
-        );
-      });
-
-      test("Should return 'hi' text when DeviceLanguage.hindi", () {
-        expect(
-          DeviceLanguage.hindi.getLanguageCode(),
-          "hi",
-        );
-      });
-
-      test("Should return 'ru' text when DeviceLanguage.russian", () {
-        expect(
-          DeviceLanguage.russian.getLanguageCode(),
-          "ru",
-        );
-      });
-
-      test("Should return 'zh' text when DeviceLanguage.chinese", () {
-        expect(
-          DeviceLanguage.chinese.getLanguageCode(),
-          "zh",
-        );
-      });
+    test("Should return Indian flag image when DeviceLanguage.hindi", () {
+      expect(
+        DeviceLanguage.hindi.getImage(),
+        const AssetImage(ImageRaster.flagIndia),
+      );
     });
 
-    group("toLocale", () {
-      test("Should return Locale(ar) when DeviceLanguage.arabic", () {
-        expect(
-          DeviceLanguage.arabic.toLocale(),
-          const Locale("ar"),
-        );
-      });
+    test("Should return Indonesian flag image when DeviceLanguage.indonesian",
+        () {
+      expect(
+        DeviceLanguage.indonesian.getImage(),
+        const AssetImage(ImageRaster.flagIndonesia),
+      );
+    });
 
-      test("Should return Locale(en) when DeviceLanguage.english", () {
-        expect(
-          DeviceLanguage.english.toLocale(),
-          const Locale("en"),
-        );
-      });
+    test("Should return Russian flag image when DeviceLanguage.russian", () {
+      expect(
+        DeviceLanguage.russian.getImage(),
+        const AssetImage(ImageRaster.flagRussia),
+      );
+    });
 
-      test("Should return Locale(es) when DeviceLanguage.spanish", () {
-        expect(
-          DeviceLanguage.spanish.toLocale(),
-          const Locale("es"),
-        );
-      });
+    test("Should return Chinese flag image when DeviceLanguage.chinese", () {
+      expect(
+        DeviceLanguage.chinese.getImage(),
+        const AssetImage(ImageRaster.flagChina),
+      );
+    });
+  });
 
-      test("Should return Locale(hi) when DeviceLanguage.hindi", () {
-        expect(
-          DeviceLanguage.hindi.toLocale(),
-          const Locale("hi"),
-        );
-      });
+  group(".getLanguageCode()", () {
+    test("Should return 'ar' text when DeviceLanguage.arabic", () {
+      expect(
+        DeviceLanguage.arabic.getLanguageCode(),
+        "ar",
+      );
+    });
 
-      test("Should return Locale(id) when DeviceLanguage.indonesian", () {
-        expect(
-          DeviceLanguage.indonesian.toLocale(),
-          const Locale("id"),
-        );
-      });
+    test("Should return 'en' text when DeviceLanguage.english", () {
+      expect(
+        DeviceLanguage.english.getLanguageCode(),
+        "en",
+      );
+    });
 
-      test("Should return Locale(ru) when DeviceLanguage.russian", () {
-        expect(
-          DeviceLanguage.russian.toLocale(),
-          const Locale("ru"),
-        );
-      });
+    test("Should return 'es' text when DeviceLanguage.spanish", () {
+      expect(
+        DeviceLanguage.spanish.getLanguageCode(),
+        "es",
+      );
+    });
 
-      test("Should return Locale(zh) when DeviceLanguage.chinese", () {
-        expect(
-          DeviceLanguage.chinese.toLocale(),
-          const Locale("zh"),
-        );
-      });
+    test("Should return 'hi' text when DeviceLanguage.hindi", () {
+      expect(
+        DeviceLanguage.hindi.getLanguageCode(),
+        "hi",
+      );
+    });
+
+    test("Should return 'ru' text when DeviceLanguage.russian", () {
+      expect(
+        DeviceLanguage.russian.getLanguageCode(),
+        "ru",
+      );
+    });
+
+    test("Should return 'zh' text when DeviceLanguage.chinese", () {
+      expect(
+        DeviceLanguage.chinese.getLanguageCode(),
+        "zh",
+      );
+    });
+  });
+
+  group(".getName()", () {
+    test("Should return 'Arabic' text when DeviceLanguage.arabic", () {
+      expect(
+        DeviceLanguage.arabic.getName(),
+        "Arabic",
+      );
+    });
+
+    test("Should return 'English' text when DeviceLanguage.english", () {
+      expect(
+        DeviceLanguage.english.getName(),
+        "English",
+      );
+    });
+
+    test("Should return 'Spanish' text when DeviceLanguage.spanish", () {
+      expect(
+        DeviceLanguage.spanish.getName(),
+        "Spanish",
+      );
+    });
+
+    test("Should return 'Hindi' text when DeviceLanguage.hindi", () {
+      expect(
+        DeviceLanguage.hindi.getName(),
+        "Hindi",
+      );
+    });
+
+    test("Should return 'Russian' text when DeviceLanguage.russian", () {
+      expect(
+        DeviceLanguage.russian.getName(),
+        "Russian",
+      );
+    });
+
+    test("Should return 'Chinese, Simplified' text when DeviceLanguage.chinese",
+        () {
+      expect(
+        DeviceLanguage.chinese.getName(),
+        "Chinese, Simplified",
+      );
+    });
+  });
+
+  group(".getNativeName()", () {
+    test("Should return 'عربي' text when DeviceLanguage.arabic", () {
+      expect(
+        DeviceLanguage.arabic.getNativeName(),
+        "عربي",
+      );
+    });
+
+    test("Should return 'English' text when DeviceLanguage.english", () {
+      expect(
+        DeviceLanguage.english.getNativeName(),
+        "English",
+      );
+    });
+
+    test("Should return 'Español' text when DeviceLanguage.spanish", () {
+      expect(
+        DeviceLanguage.spanish.getNativeName(),
+        "Español",
+      );
+    });
+
+    test("Should return 'हिन्दी' text when DeviceLanguage.hindi", () {
+      expect(
+        DeviceLanguage.hindi.getNativeName(),
+        "हिन्दी",
+      );
+    });
+
+    test("Should return 'Русский' text when DeviceLanguage.russian", () {
+      expect(
+        DeviceLanguage.russian.getNativeName(),
+        "Русский",
+      );
+    });
+
+    test("Should return '简体中文' text when DeviceLanguage.chinese", () {
+      expect(
+        DeviceLanguage.chinese.getNativeName(),
+        "简体中文",
+      );
+    });
+  });
+
+  group(".toLocale()", () {
+    test("Should return Locale(ar) when DeviceLanguage.arabic", () {
+      expect(
+        DeviceLanguage.arabic.toLocale(),
+        const Locale("ar"),
+      );
+    });
+
+    test("Should return Locale(en) when DeviceLanguage.english", () {
+      expect(
+        DeviceLanguage.english.toLocale(),
+        const Locale("en"),
+      );
+    });
+
+    test("Should return Locale(es) when DeviceLanguage.spanish", () {
+      expect(
+        DeviceLanguage.spanish.toLocale(),
+        const Locale("es"),
+      );
+    });
+
+    test("Should return Locale(hi) when DeviceLanguage.hindi", () {
+      expect(
+        DeviceLanguage.hindi.toLocale(),
+        const Locale("hi"),
+      );
+    });
+
+    test("Should return Locale(id) when DeviceLanguage.indonesian", () {
+      expect(
+        DeviceLanguage.indonesian.toLocale(),
+        const Locale("id"),
+      );
+    });
+
+    test("Should return Locale(ru) when DeviceLanguage.russian", () {
+      expect(
+        DeviceLanguage.russian.toLocale(),
+        const Locale("ru"),
+      );
+    });
+
+    test("Should return Locale(zh) when DeviceLanguage.chinese", () {
+      expect(
+        DeviceLanguage.chinese.toLocale(),
+        const Locale("zh"),
+      );
     });
   });
 }
