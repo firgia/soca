f08c75db70933350b763917f9e20d983521e081b
Mochamad Firgia <mochamad@firgia.com>
2023-03-04 15:35:53 +0700

    fixing locale extension test

--- test/core/utility/extension/locale_extension_test.dart
+++ test/core/utility/extension/locale_extension_test.dart
@@ -12,56 +12,54 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:soca/core/core.dart';
 
 void main() {
-  group("Functions", () {
-    group("toDeviceLanguage", () {
-      test("Should return DeviceLanguage.arabic when Locale(ar)", () {
-        expect(
-          const Locale("ar").toDeviceLanguage(),
-          DeviceLanguage.arabic,
-        );
-      });
+  group(".toDeviceLanguage()", () {
+    test("Should return DeviceLanguage.arabic when Locale(ar)", () {
+      expect(
+        const Locale("ar").toDeviceLanguage(),
+        DeviceLanguage.arabic,
+      );
+    });
 
-      test("Should return DeviceLanguage.english when Locale(en)", () {
-        expect(
-          const Locale("en").toDeviceLanguage(),
-          DeviceLanguage.english,
-        );
-      });
+    test("Should return DeviceLanguage.english when Locale(en)", () {
+      expect(
+        const Locale("en").toDeviceLanguage(),
+        DeviceLanguage.english,
+      );
+    });
 
-      test("Should return DeviceLanguage.spanish when Locale(es)", () {
-        expect(
-          const Locale("es").toDeviceLanguage(),
-          DeviceLanguage.spanish,
-        );
-      });
+    test("Should return DeviceLanguage.spanish when Locale(es)", () {
+      expect(
+        const Locale("es").toDeviceLanguage(),
+        DeviceLanguage.spanish,
+      );
+    });
 
-      test("Should return DeviceLanguage.hindi when Locale(hi)", () {
-        expect(
-          const Locale("hi").toDeviceLanguage(),
-          DeviceLanguage.hindi,
-        );
-      });
+    test("Should return DeviceLanguage.hindi when Locale(hi)", () {
+      expect(
+        const Locale("hi").toDeviceLanguage(),
+        DeviceLanguage.hindi,
+      );
+    });
 
-      test("Should return DeviceLanguage.indonesian when Locale(id)", () {
-        expect(
-          const Locale("id").toDeviceLanguage(),
-          DeviceLanguage.indonesian,
-        );
-      });
+    test("Should return DeviceLanguage.indonesian when Locale(id)", () {
+      expect(
+        const Locale("id").toDeviceLanguage(),
+        DeviceLanguage.indonesian,
+      );
+    });
 
-      test("Should return DeviceLanguage.russian when Locale(ru)", () {
-        expect(
-          const Locale("ru").toDeviceLanguage(),
-          DeviceLanguage.russian,
-        );
-      });
+    test("Should return DeviceLanguage.russian when Locale(ru)", () {
+      expect(
+        const Locale("ru").toDeviceLanguage(),
+        DeviceLanguage.russian,
+      );
+    });
 
-      test("Should return DeviceLanguage.chinese when Locale(zh)", () {
-        expect(
-          const Locale("zh").toDeviceLanguage(),
-          DeviceLanguage.chinese,
-        );
-      });
+    test("Should return DeviceLanguage.chinese when Locale(zh)", () {
+      expect(
+        const Locale("zh").toDeviceLanguage(),
+        DeviceLanguage.chinese,
+      );
     });
   });
 }
