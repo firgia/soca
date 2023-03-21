7d05cd1d62b43a40b831675259b50300c8d98b3c
Mochamad Firgia <mochamad@firgia.com>
2023-03-21 19:00:00 +0700

    add gender age chip widget

---
+++ lib/presentation/widgets/chip/chip.dart
@@ -0,0 +1,10 @@
+/*
+ * Author     : Mochamad Firgia
+ * Website    : https://www.firgia.com
+ * Repository : https://github.com/firgia/soca
+ * 
+ * Created on Mon Mar 20 2023
+ * Copyright (c) 2023 Mochamad Firgia
+ */
+
+export 'gender_age_chip.dart';
