8d6892d12c898a4fab60906f3b3c4d724d3ad339
Mochamad Firgia <mochamad@firgia.com>
2023-05-05 21:48:19 +0700

    remove unused call kit on call action bloc

--- test/logic/bloc/call_action/call_action_bloc_test.dart
+++ test/logic/bloc/call_action/call_action_bloc_test.dart
@@ -8,10 +8,8 @@
  */
 
 import 'package:bloc_test/bloc_test.dart';
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
-import 'package:soca/config/config.dart';
 import 'package:soca/core/core.dart';
 import 'package:soca/data/data.dart';
 import 'package:soca/logic/logic.dart';
@@ -21,13 +19,11 @@ import '../../../mock/mock.mocks.dart';
 
 void main() {
   late MockCallingRepository callingRepository;
-  late MockCallKit callKit;
   late MockUserRepository userRepository;
 
   setUp(() {
     registerLocator();
     callingRepository = getMockCallingRepository();
-    callKit = getMockCallKit();
     userRepository = getMockUserRepository();
   });
 
@@ -347,12 +343,6 @@ void main() {
               role: RTCRole.publisher,
               userType: UserType.blind,
             ),
-            callKit.startCall(CallKitArgument(
-              id: call.id,
-              nameCaller: volunteerUser.name,
-              handle: LocaleKeys.volunteer.tr(),
-              type: 1,
-            )),
           ]);
         },
       );
